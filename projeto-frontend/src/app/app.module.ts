72c682763bd96037e0ce5b637f28f7c1b9baad1a
Luana Paes <luanafranca598@gmail.com>
2024-03-24 00:20:19 -0300

    Criação dos componentes principais do projeto.
    - Criação dos componentes:
    - button-edit, button-delete, button-add e table.

--- projeto-frontend/src/app/app.module.ts
+++ projeto-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { ButtonEditComponent } from './components/button-edit/button-edit.component';
+import { ButtonDeleteComponent } from './components/button-delete/button-delete.component';
+import { ButtonAddComponent } from './components/button-add/button-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    ButtonEditComponent,
+    ButtonDeleteComponent,
+    ButtonAddComponent
   ],
   imports: [
     BrowserModule,
